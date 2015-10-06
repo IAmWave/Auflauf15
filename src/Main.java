828532d8adf96c4449cce844dda00a64818b76c2
Václav Volhejn <vaclav.volhejn@gmail.com>
2015-10-06 13:45:09 +0200

    Záchrana při zaseknutí

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,25 @@
 import controller.Controller;
 import controller.RobotController;
 import lejos.nxt.Button;
+import lejos.nxt.Sound;
 import model.Exploration;
 import model.Move;
 
 public class Main {
 
     public static void main(String[] args) {
-        go();
+        new Thread() {
+            public void run() {
+                while (true) {
+                    if (Button.readButtons() != 0) {
+                        System.exit(0);
+                    }
+                    Thread.yield();
+                }
+            }
+        }.start();
+        new RobotController(null).move(2);
+        //go();
     }
 
     public static void go() { //hlavní metoda
@@ -16,7 +28,6 @@ public class Main {
         Controller c = new RobotController(e);
 
         //todo: vyjet ze startu před cyklem
-        
         while (Button.readButtons() == 0) {
             Move next = e.decide();
             c.turn(e.getRotation().rotationTo(next.dir));
@@ -24,6 +35,7 @@ public class Main {
             //move bude zaroven prubezne hlasit Exploreru data ze skenu a pripadne kde narazil.
             //Hašení se přenechává výhradně RobotControlleru.
             c.move(next.tiles);
+            Sound.twoBeeps();
             //s aktualizovanymi daty zacne cyklus znovu
         }
     }
