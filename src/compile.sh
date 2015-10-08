001bafd3ac601d8c3f24af8e24f1d31c69e810fa
Colanderr <jezek.malybuk@seznam.cz>
2015-10-08 10:13:18 +0200

    Temer funkcni pohyb, linuxi compile script

---
+++ src/compile.sh
@@ -0,0 +1,6 @@
+nxjc Main.java
+nxjc */*.java
+nxjlink -o Main.nxj Main
+rm *.class
+rm */*.class
+sudo env "PATH=$PATH" nxjupload Main.nxj
--- src/controller/RobotController.java
+++ src/controller/RobotController.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
+import lejos.util.Delay;
 import model.ai.Exploration;
 
 /**
@@ -14,26 +10,107 @@ import model.ai.Exploration;
  * @author Václav
  */
 public class RobotController implements Controller {
+
     NXTRegulatedMotor sensor = Motor.A;
     NXTRegulatedMotor left = Motor.B;
     NXTRegulatedMotor right = Motor.C;
-    
+    final int maxSpeed = 600;
+    final int minSpeed = 100;
+
     Exploration exp;
-    
-    public RobotController(Exploration exp){
-        left.setSpeed(360);
-        right.setSpeed(360);
+
+    public RobotController(Exploration exp) {
+        //System.out.println("LEL");
+        //for (int i = 0; i < 12; i++) {
+        //turn(1);
+        //}
+        //Delay.msDelay(5000);
+
         this.exp = exp;
     }
-    
-    public void move(int tiles){
-        left.rotate(-720, true);
-        right.rotate(-720, true);
+
+    public void move(int tiles) {
+        left.rotate(-400 * tiles, true);
+        right.rotate(-400 * tiles, true);
+        int deg = left.getTachoCount();
+        int targetDeg = deg - 400 * tiles;
+        for (int i = 1; i < this.maxSpeed; i += 20) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
+            Motor.B.setSpeed(i);
+            Motor.C.setSpeed(i);
+            Delay.msDelay(20);
+        }
+        left.setSpeed(this.maxSpeed);
+        right.setSpeed(this.maxSpeed);
+        //Delay.msDelay(1000);
+        int l = 0;
+        System.out.println(targetDeg - left.getTachoCount());
+        while (Math.abs(targetDeg - left.getTachoCount()) > 200) {
+            System.out.println(targetDeg - left.getTachoCount());
+            Delay.msDelay(20);
+            l++;
+            if (l > 1000) {
+                break;
+            }
+        }
+        for (int i = 1; i < this.maxSpeed; i += 30) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
+            Motor.B.setSpeed(this.maxSpeed - i);
+            Motor.C.setSpeed(this.maxSpeed - i);
+            System.out.println(left.getSpeed());
+            if (this.maxSpeed - i <= this.minSpeed) {
+                Motor.B.setSpeed(this.minSpeed);
+                Motor.C.setSpeed(this.minSpeed);
+                break;
+            }
+            Delay.msDelay(20);
+        }
+        long time = System.currentTimeMillis();
+        while (left.isMoving()) {
+            Delay.msDelay(10);
+            if (System.currentTimeMillis() - time > 15000) {
+                break;
+            }
+        }
         //v budoucnu: skenovat, výsledek nahlásit exp
     }
-    
-    public void turn(int times){//times>0 => doprava
-        left.rotate(-720, true);
-        right.rotate(720, true);
+
+    public void turn(int times) {//times>0 => doprava
+        int targetDeg = left.getTachoCount() + 180 * times;
+        left.rotate(180, true);
+        right.rotate(-180, true);
+        for (int i = 1; i < this.maxSpeed; i += 25) {
+            left.setSpeed(i);
+            right.setSpeed(i);
+            Delay.msDelay(10);
+        }
+        long time = System.currentTimeMillis();
+        System.out.println("ACCELERATED " + Math.abs(targetDeg - left.getTachoCount()));
+
+        while (Math.abs(targetDeg - left.getTachoCount()) > 100) {
+            System.out.println(targetDeg - left.getTachoCount());
+            Delay.msDelay(20);
+            if (System.currentTimeMillis() - time > 15000) {
+                break;
+            }
+        }
+        
+        for (int i = 1; i < this.maxSpeed; i += 50) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
+            Motor.B.setSpeed(this.maxSpeed - i);
+            Motor.C.setSpeed(this.maxSpeed - i);
+            System.out.println(left.getSpeed());
+            if (this.maxSpeed - i <= this.minSpeed) {
+                Motor.B.setSpeed(this.minSpeed);
+                Motor.C.setSpeed(this.minSpeed);
+                break;
+            }
+            Delay.msDelay(20);
+        }
+        
+        time = System.currentTimeMillis();
+        while (left.isMoving()) {
+            Delay.msDelay(10);
+            if (System.currentTimeMillis() - time > 15000) {
+                break;
+            }
+        }
     }
 }
