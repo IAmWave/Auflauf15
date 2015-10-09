0134d206b0617b329d3ec84454e006a55af652bb
Colanderr <jezek.malybuk@seznam.cz>
2015-10-09 10:43:20 +0200

    sensory

--- src/controller/RobotController.java
+++ src/controller/RobotController.java
@@ -3,6 +3,8 @@ package controller;
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
 import lejos.util.Delay;
 import model.Exploration;
 
@@ -11,19 +13,22 @@ import model.Exploration;
  * @author Václav
  */
 public class RobotController implements Controller {
-
-    NXTRegulatedMotor sensor = Motor.A;
+    
+    TouchSensor touch = new TouchSensor(SensorPort.S2);
+    NXTRegulatedMotor magnet = Motor.A;
     NXTRegulatedMotor left = Motor.B;
     NXTRegulatedMotor right = Motor.C;
-    final int maxSpeed = 600;
-    final int minSpeed = 100;
+    final int MAX_SPEED = 600;
+    final int MIN_SPEED = 100;
+    final int DEG_TILE = 400;
+    final int DEG_TURN = 180;
 
     Exploration exp;
 
     public RobotController(Exploration exp) {
         //System.out.println("LEL");
         //for (int i = 0; i < 12; i++) {
-        //turn(1);
+        move(3);
         //}
         //Delay.msDelay(5000);
 
@@ -31,54 +36,63 @@ public class RobotController implements Controller {
     }
 
     public void move(int tiles) {
-        left.rotate(-400 * tiles, true);
-        right.rotate(-400 * tiles, true);
+        left.rotate(-DEG_TILE * tiles, true);
+        right.rotate(-DEG_TILE * tiles, true);
         int deg = left.getTachoCount();
-        int targetDeg = deg - 400 * tiles;
-        for (int i = 1; i < this.maxSpeed; i += 20) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
-            Motor.B.setSpeed(i);
-            Motor.C.setSpeed(i);
+        int targetDeg = deg - DEG_TILE * tiles;
+        left.setSpeed(MAX_SPEED);
+        right.setSpeed(MAX_SPEED);
+        while (left.isMoving()&& !touch.isPressed()) {
             Delay.msDelay(20);
         }
-        left.setSpeed(this.maxSpeed);
-        right.setSpeed(this.maxSpeed);
-        //Delay.msDelay(1000);
-        int l = 0;
-        System.out.println(targetDeg - left.getTachoCount());
-        while (Math.abs(targetDeg - left.getTachoCount()) > 200) {
-            System.out.println(targetDeg - left.getTachoCount());
-            Delay.msDelay(20);
-            l++;
-            if (l > 1000) {
-                break;
-            }
-        }
-        for (int i = 1; i < this.maxSpeed; i += 30) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
-            Motor.B.setSpeed(this.maxSpeed - i);
-            Motor.C.setSpeed(this.maxSpeed - i);
-            System.out.println(left.getSpeed());
-            if (this.maxSpeed - i <= this.minSpeed) {
-                Motor.B.setSpeed(this.minSpeed);
-                Motor.C.setSpeed(this.minSpeed);
-                break;
-            }
-            Delay.msDelay(20);
-        }
-        long time = System.currentTimeMillis();
-        while (left.isMoving()) {
-            Delay.msDelay(10);
-            if (System.currentTimeMillis() - time > 15000) {
-                break;
-            }
-        }
+        left.flt();
+        right.flt();
+        System.out.println("ADWADAWD");
+        /*
+         for (int i = 1; i < this.MAX_SPEED; i += 20) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
+         Motor.B.setSpeed(i);
+         Motor.C.setSpeed(i);
+         Delay.msDelay(20);
+         }
+         left.setSpeed(this.MAX_SPEED);
+         right.setSpeed(this.MAX_SPEED);
+         //Delay.msDelay(1000);
+         int l = 0;
+         System.out.println(targetDeg - left.getTachoCount());
+         while (Math.abs(targetDeg - left.getTachoCount()) > 200) {
+         System.out.println(targetDeg - left.getTachoCount());
+         Delay.msDelay(20);
+         l++;
+         if (l > 1000) {
+         break;
+         }
+         }
+         for (int i = 1; i < this.MAX_SPEED; i += 30) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
+         Motor.B.setSpeed(this.MAX_SPEED - i);
+         Motor.C.setSpeed(this.MAX_SPEED - i);
+         System.out.println(left.getSpeed());
+         if (this.MAX_SPEED - i <= this.MIN_SPEED) {
+         Motor.B.setSpeed(this.MIN_SPEED);
+         Motor.C.setSpeed(this.MIN_SPEED);
+         break;
+         }
+         Delay.msDelay(20);
+         }
+         long time = System.currentTimeMillis();
+         while (left.isMoving()) {
+         Delay.msDelay(10);
+         if (System.currentTimeMillis() - time > 15000) {
+         break;
+         }
+         }*/
         //v budoucnu: skenovat, výsledek nahlásit exp
     }
 
     public void turn(int times) {//times>0 => doprava
-        int targetDeg = left.getTachoCount() + 180 * times;
-        left.rotate(180, true);
-        right.rotate(-180, true);
-        for (int i = 1; i < this.maxSpeed; i += 25) {
+        int targetDeg = left.getTachoCount() + DEG_TURN * times;
+        left.rotate(DEG_TURN, true);
+        right.rotate(-DEG_TURN, true);
+        for (int i = 1; i < this.MAX_SPEED; i += 25) {
             left.setSpeed(i);
             right.setSpeed(i);
             Delay.msDelay(10);
@@ -93,19 +107,19 @@ public class RobotController implements Controller {
                 break;
             }
         }
-        
-        for (int i = 1; i < this.maxSpeed; i += 50) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
-            Motor.B.setSpeed(this.maxSpeed - i);
-            Motor.C.setSpeed(this.maxSpeed - i);
+
+        for (int i = 1; i < this.MAX_SPEED; i += 50) {//LEJOS JE RETARDOVANEJ A NEFUNGUJE 0
+            Motor.B.setSpeed(this.MAX_SPEED - i);
+            Motor.C.setSpeed(this.MAX_SPEED - i);
             System.out.println(left.getSpeed());
-            if (this.maxSpeed - i <= this.minSpeed) {
-                Motor.B.setSpeed(this.minSpeed);
-                Motor.C.setSpeed(this.minSpeed);
+            if (this.MAX_SPEED - i <= this.MIN_SPEED) {
+                Motor.B.setSpeed(this.MIN_SPEED);
+                Motor.C.setSpeed(this.MIN_SPEED);
                 break;
             }
             Delay.msDelay(20);
         }
-        
+
         time = System.currentTimeMillis();
         while (left.isMoving()) {
             Delay.msDelay(10);
