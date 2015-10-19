346d2df35b0806f2b4a3570643706068b5f9f53a
Colanderr <jezek.malybuk@seznam.cz>
2015-10-19 12:33:56 +0200

    Uprava kompilovaciho skriptu pro linux, aby kompiloval do Auflauf15.nxj

--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,7 @@
 nxjc Main.java
 nxjc */*.java
-nxjlink -o Main.nxj Main
+nxjlink -o Auflauf15.nxj Main
 rm *.class
 rm */*.class
-sudo env "PATH=$PATH" nxjupload Main.nxj
+sudo env "PATH=$PATH" nxjupload Auflauf15.nxj
+rm Auflauf15.nxj
